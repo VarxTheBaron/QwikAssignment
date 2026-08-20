a24b590caf151f6ab96950394986646e9d67b689
Jonas Persson <jonas.persson111@gmail.com>
2026-08-20 18:40:17 +0200

    updated footer and footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,14 @@ import styles from "./footer.module.css";
 export default component$(() => {
   return (
     <footer class={styles.footer_container}>
-      All images are AI generated. © Jonas Persson 2026
+      <ul class={styles.footer_list}>
+        {FooterItem("All images are AI generated.")}
+        {FooterItem("© Jonas Persson 2026")}
+      </ul>
     </footer>
   );
 });
+
+export const FooterItem = (text: string) => {
+  return <li class={styles.footer_item}>{text}</li>;
+};

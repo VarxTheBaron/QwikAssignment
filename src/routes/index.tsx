2514e59e2a3915b0cc394d8028d23dffdce37ae3
Jonas Persson <jonas.persson111@gmail.com>
2026-08-24 18:28:38 +0200

    updated start page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,42 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import styles from "./index.module.css";
+import WallpaperGrid from "~/components/wallpaperGrid";
+import { startPageWallpapers } from "~/data/wallpapers";
 
 export default component$(() => {
   return (
-    <main>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+    <main class={styles.main}>
+      <Greeter />
+      <WallpaperGrid wallpapers={startPageWallpapers()} />
+      <InfoCard
+        title="Wallpaper stats"
+        text="
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, eligendi. Blanditiis, alias! Animi obcaecati nam id. Dolores doloribus beatae aliquam reiciendis quasi mollitia ex obcaecati laudantium delectus veritatis! Nesciunt, ab!"
+      />
     </main>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Awesome Wallpapers",
   meta: [
     {
       name: "description",
       content: "Qwik site description",
     },
   ],
+};
+
+export const Greeter = () => {
+  return <h1 class={styles.greeter}>The best wallpapers ever!</h1>;
+};
+
+export const InfoCard = (props: { title: string; text: string }) => {
+  return (
+    <section class={styles.statsInfo}>
+      <h2>{props.title}</h2>
+      <p>{props.text}</p>
+    </section>
+  );
 };

2eb7bac485411e39dabb58f7419e0f888e268f3e
Jonas Persson <jonas.persson111@gmail.com>
2026-08-25 17:55:14 +0200

    updated navigation comp and added own css module

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,19 +1,15 @@
 import { component$ } from "@builder.io/qwik";
-import styles from "./header.module.css";
+import styles from "./navigation.module.css";
 
 export default component$(() => {
   return (
     <nav>
       <a href="/">
-        <img src="/wallpapersLogoSmall.png" alt="Company Logo" />
+        <img src="/wallpapersLogoSmall.png" alt="Awesome wallpapers home" />
       </a>
       <div class={styles.navigation}>
-        <a href="/favorites">
-          <span>Favorites</span>
-        </a>
-        <a href="/random">
-          <span>Random</span>
-        </a>
+        <a href="/favorites">Favorites</a>
+        <a href="/random">Random</a>
       </div>
     </nav>
   );

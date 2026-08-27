e3f8a9f769460e04f5d4d206badf630337721735
Jonas Persson <jonas.persson111@gmail.com>
2026-08-27 15:32:27 +0200

    changed signal to store

--- src/routes/favorites/index.tsx
+++ src/routes/favorites/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import { component$, useStore, useVisibleTask$ } from "@builder.io/qwik";
 import styles from "./index.module.css";
 import WallpaperGrid from "~/components/wallpaperGrid";
 import { getFavoriteWallpapers } from "~/data/favorites";
@@ -6,19 +6,16 @@ import { type Wallpaper } from "~/data/wallpapers";
 import { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  const favoriteWallpapers = useSignal<Wallpaper[]>([]);
+  const favoriteWallpapers = useStore<Wallpaper[]>([]);
 
   useVisibleTask$(() => {
-    favoriteWallpapers.value = getFavoriteWallpapers();
+    favoriteWallpapers.push(...getFavoriteWallpapers());
   });
 
   return (
     <main class={styles.main}>
-      {favoriteWallpapers.value.length > 0 && (
-        <WallpaperGrid
-          wallpapers={favoriteWallpapers.value}
-          title="Favorites"
-        />
+      {favoriteWallpapers.length > 0 && (
+        <WallpaperGrid wallpapers={favoriteWallpapers} title="Favorites" />
       )}
     </main>
   );

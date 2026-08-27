4080dea598188b7a58ee0ede9fd99a05e3c43a00
Jonas Persson <jonas.persson111@gmail.com>
2026-08-27 16:09:04 +0200

    updated searchbox

--- src/components/searchBox.tsx
+++ src/components/searchBox.tsx
@@ -1,17 +1,26 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore } from "@builder.io/qwik";
 import styles from "./searchBox.module.css";
+import { useNavigate } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const formState = useStore({ query: "" });
+  const navigate = useNavigate();
+
   return (
     <div class={styles.searchBoxContainer}>
-      <form action="/search" method="GET" class={styles.form}>
+      <form
+        preventdefault:submit
+        class={styles.form}
+        onSubmit$={() => navigate(`/search?q=${formState.query}`)}
+      >
         <label for="search">Search:</label>
         <input
           id="search"
           class={styles.searchField}
           type="search"
-          name="q"
           placeholder="Category or title"
+          onInput$={(event, element) => (formState.query = element.value)}
+          value={formState.query}
         />
         <button
           type="submit"
